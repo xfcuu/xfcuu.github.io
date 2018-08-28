30863822defe515a6d4c1879b91f410040dea809
Kwame Adoko <blakroku@outlook.com>
2018-08-29 05:07:42 +0800

    Initial commit

---
+++ iframes/Login/Login.js
@@ -0,0 +1,25 @@
+﻿
+(function ($) {
+
+    this.init = function () {
+        this.login();
+    };
+
+    /****************************************************************
+     LOGIN
+    ****************************************************************/
+    this.login = function (e) {
+        $("input").focus(function () {
+            $(this).siblings("label").hide();
+        });
+        $("input").blur(function () {
+            if ($(this).val() == "") {
+                $(this).siblings("label").show();
+            }
+        });
+    };
+
+    this.init();
+
+})(jQuery);
+
